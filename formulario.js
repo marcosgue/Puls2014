fba998b6ad6bf912ea806f0805ceb3d28740cfe0
Marcos <marcos@marcos-MS-7693.(none)>
2014-04-02 18:18:44 -0300

    Terminada clase 5

--- formulario.js
+++ formulario.js
@@ -5,16 +5,26 @@ var $form = $('#formulario'),
 	$list = $('#contenido'),
 	$post = $('.item').first();
 
+if (localStorage.getItem('autosave')){
+	$titulo.val(sessionStorage.getItem('titulo'));
+	$url.val(sessionStorage.getItem('url'));
+}
 
-function mostrarFormulario(){
+var id = setInterval(function(){
+	sessionStorage.setItem('titulo', $titulo.val());
+	sessionStorage.setItem('url', $url.val());
+}, 1000)
+
+function mostrarOcultarFormulario(){
 	$form.slideToggle();
+	$lista.slideToggle();
 	return false;
 }
 
 function agregarPost(){
 	var url = $url.val(),
 		titulo = $titulo.val(),
-		$clone = $post.clone();
+		clone = $post.clone();
 
 	$clone.find('.titulo_item a')
 		.text(titulo)
@@ -23,12 +33,14 @@ function agregarPost(){
 	$clone.hide();
 
 	$list.prepend($clone);
-
+	mostrarOcultarFormulario();
+	$titulo.val('')
+	$url.val('')
 	$clone.fadeIn();
 
 	return false;
 }
 
 // Eventos
-$button.click( mostrarFormulario );
+$button.click( mostrarOcultarFormulario );
 $form.on('submit', agregarPost );
